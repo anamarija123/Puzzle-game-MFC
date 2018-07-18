63845fa0731eeaf23952cbc0c41610db1e90ae23
anama <anama@DESKTOP-KVCBJ84>
2018-07-18 17:02:18 +0200

    Add project files.

---
+++ Puzzle Game MFC/PuzzlePictureControl.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "PuzzlePictureControl.h"
+
+
+CPuzzlePictureControl::CPuzzlePictureControl()
+{
+}
+
+CPuzzlePictureControl::~CPuzzlePictureControl()
+{
+}
+
+void CPuzzlePictureControl::ChangePicture(const LPCTSTR picturePath)
+{
+	CImage image;
+	image.Load(picturePath);
+
+	CDC* cdc = this->GetDC();
+	
+	RECT rect;
+	this->GetClientRect(&rect);
+
+	image.StretchBlt(cdc->GetSafeHdc(), rect);
+
+	CBitmap bitmap;
+	bitmap.Attach(image.Detach());
+	
+	
+	this->SetBitmap((HBITMAP) bitmap);
+}
---
+++ Puzzle Game MFC/PuzzlePictureControl.h
@@ -0,0 +1,10 @@
+#pragma once
+class CPuzzlePictureControl : public CStatic
+{
+public:
+	CPuzzlePictureControl();
+	virtual ~CPuzzlePictureControl();
+
+	void ChangePicture(const LPCTSTR picturePath);
+};
+
