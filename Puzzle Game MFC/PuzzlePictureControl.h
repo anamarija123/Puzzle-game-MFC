40df6e38459b15dd393ad4707daaf6c473768ca6
Ana-Marija Kostanjčar <anamarija.kostanjcar@gmail.com>
2018-08-11 14:22:07 +0200

    deleting old project

--- Puzzle Game MFC/PuzzlePictureControl.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class CPuzzlePictureControl : public CStatic
-{
-public:
-	CPuzzlePictureControl();
-	virtual ~CPuzzlePictureControl();
-	void ChangePicture(const LPCTSTR picturePath);
-};
-
