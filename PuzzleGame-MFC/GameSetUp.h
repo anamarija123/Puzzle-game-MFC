ce71949b3a5a14f7ae9dced5143e9ee7de84f25a
anama <anama@DESKTOP-KVCBJ84>
2018-08-28 13:40:46 +0200

    update

--- PuzzleGame-MFC/GameSetUp.h
+++ PuzzleGame-MFC/GameSetUp.h
@@ -2,7 +2,7 @@
 #include<string.h>
 #include <vector>
 #include <iterator>
-#include "PointOperators.h"
+
 
 using namespace std;
 class CGameSetUp
@@ -13,13 +13,10 @@ public:
 	void initializeParameters(int height, int width, wstring picturePath);
 	bool Shuffle();
 	void DrawPieces(HDC HwINdC);
-	void DrawGrid(CPaintDC* dc, RECT rect, HWND hwnd, HDC HwINdC);
 	void Swap(std::vector<POINT>gamerClickCoords);
-	//bool LoadBitmapPicture(LPCWSTR szFileName);
 	bool LoadBitmapPicture(HDC HwINdC, LPCWSTR szFileName);
 	bool ShowPicture(HDC HwINdC);
 	
-	std::vector<POINT>copyOriginalCoords;
 
 	const int GetPictureWidth(void) { return m_pictureWidth; }
 	const int GetPictureHeight(void) { return m_pictureHeight; }
